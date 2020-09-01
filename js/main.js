ea885845b2a9ec3b8fa3b7b298851b12b0204a14
Jimmy Hooker <jimmyjjwalker@gmail.com>
2020-09-01 00:54:10 -0700

    Figured it out

--- js/main.js
+++ js/main.js
@@ -1,123 +1,129 @@
+// import "@babel/polyfill";
 
-let columns = 20;
-let rows = 20;
+let columns = window.innerWidth / 3;
+let rows = window.innerHeight / 3;
 
 let pixels = [];
+
+let state = {
+  iteration: 0,
+  running: true
+}
 
 const initialSeed = () => {
   for (let i = 0; i < columns; i++) {
     pixels.push([]);
     for (let j = 0; j < rows; j++) {
-      // pixels[i][j] = Math.random() >= 0.5;
-      pixels[i][j] = false;
+      pixels[i][j] = Math.random() >= 0.5;
+      // pixels[i][j] = false;
     }
   }
   // glider
+  // pixels[1][2] = true;
+  // pixels[2][3] = true;
+  // pixels[3][1] = true;
+  // pixels[3][2] = true;
+  // pixels[3][3] = true;
+  // square
+  // pixels[0][0] = true;
+  // pixels[0][1] = true;
+  // pixels[0][2] = true;
+  // pixels[1][0] = true;
+  // pixels[1][1] = true;
+  // pixels[1][2] = true;
   // pixels[2][0] = true;
   // pixels[2][1] = true;
   // pixels[2][2] = true;
-  // pixels[1][2] = true;
-  // pixels[0][1] = true;
-  // square
-  pixels[0][0] = true;
-  pixels[0][1] = true;
-  pixels[0][2] = true;
-  pixels[1][0] = true;
-  pixels[1][1] = true;
-  pixels[1][2] = true;
-  pixels[2][0] = true;
-  pixels[2][1] = true;
-  pixels[2][2] = true;
 
   draw(pixels);
-  console.log(pixels);
-  setTimeout(function () {
-    update(pixels);
-  }, 3000);
 };
 
-
-
-const update = () => {
-  console.log(pixels);
-  let newArray = [...pixels];
-
-  // To update, take everyone in the existing pixels array
-  // Loop through, calculate whether they should now be alive or dead
-  // Assign those values to newArray
-  // Then wait a sec probably
-  // And call itself again?
-  // for (let i = 0; i < columns; i++) {
-  //   newArray.push([]);
-  //   for (let j = 0; j < rows; j++) {
-  //     let neighbors = calculateNeighbors(i, j, pixels);
-  //     console.log(neighbors);
-  //     if (neighbors < 2 || neighbors > 3) {
-  //       newArray[i][j] = false;
-  //     } else if (neighbors === 3 || neighbors === 2) {
-  //       newArray[i][j] = true;
-  //     }
-  //   }
-  // }
-  const calculateNeighbors = (x, y, array) => {
-    let neighbors = 0;
-    const isAlive = (x, y) => {
-      return array[x] && array[x][y] && array[x][y] === true;
-    };
-    if (isAlive(x - 1, y - 1)) neighbors++;
-    if (isAlive(x, y - 1)) neighbors++;
-    if (isAlive(x + 1, y - 1)) neighbors++;
-    if (isAlive(x - 1, y)) neighbors++;
-    if (isAlive(x + 1, y)) neighbors++;
-    if (isAlive(x - 1, y + 1)) neighbors++;
-    if (isAlive(x, y + 1)) neighbors++;
-    if (isAlive(x + 1, y + 1)) neighbors++;
-
-    return neighbors;
+const calculateNeighbors = (x, y, array) => {
+  let neighbors = 0;
+  const isAlive = (x, y) => {
+    return array[x] && array[x][y] && array[x][y] === true;
   };
+  if (isAlive(x, y + 1)) neighbors++;
+  if (isAlive(x, y - 1)) neighbors++;
+  if (isAlive(x + 1, y - 1)) neighbors++;
+  if (isAlive(x - 1, y + 1)) neighbors++;
+  if (isAlive(x + 1, y + 1)) neighbors++;
+  if (isAlive(x - 1, y - 1)) neighbors++;
+  if (isAlive(x + 1, y)) neighbors++;
+  if (isAlive(x - 1, y)) neighbors++;
 
-  for (let i = 0; i < newArray.length; i++) {
-    for (let j = 0; j < newArray[i].length; j++) {
-      let neighbors = calculateNeighbors(i, j, newArray);
-      if (neighbors > 5) {
-        console.log("logged it");
-        newArray[i][j] = true;
+  return neighbors;
+};
+
+const createNextArray = () => {
+  let nextArray = [];
+  for (let i = 0; i < columns; i++) {
+    nextArray.push([]);
+    for (let j = 0; j < rows; j++) {
+      let neighbors = calculateNeighbors(i, j, pixels);
+      if (pixels[i][j] === true) {
+        if (neighbors === 2 || neighbors === 3) {
+          nextArray[i][j] = true;
+        } else {
+          nextArray[i][j] = false;
+        }
+      } else if (neighbors === 3) {
+        nextArray[i][j] = true;
       } else {
-        newArray[i][j] = false;
+        nextArray[i][j] = false;
       }
-      // if (neighbors < 2 || neighbors > 3) {
-      //   // console.log(`${i, j} was ${pixels[i][j]} -> now ${pixels[i][j]}`);
-      //   newArray[i][j] = false;
-      // } else if (neighbors === 3 || neighbors === 2) {
-      //   // console.log(`${(i, j)} was ${pixels[i][j]} -> now ${pixels[i][j]}`);
-      //   newArray[i][j] = true;
-      // }
     }
   }
-
-  pixels = [...newArray];
+  // console.log(nextArray);
+  pixels = nextArray;
   draw(pixels);
+  state.iteration++;
+  const iteration = document.getElementById("iteration");
+  iteration.innerHTML = state.iteration;
+  if (state.running === true) {
+    setTimeout(function () {
+      createNextArray();
+    }, 60);
+  }
 };
 
+const stopStartButton = document.getElementById("stopStartButton");
+stopStartButton.addEventListener("click", () => {
+  if (state.running === true) {
+    state.running = false;
+    stopStartButton.innerHTML = "Start";
+  } else {
+    state.running = true;
+    stopStartButton.innerHTML = "Stop";
+    createNextArray();
+  }
+});
+
 const draw = (array) => {
+  const canvas = document.getElementById("lifecanvas");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   const ctx = document.getElementById("lifecanvas").getContext("2d");
   ctx.clearRect(0, 0, 1512, 512);
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array[i].length; j++) {
       if (array[i][j] === true) {
-        ctx.fillStyle = "green";
-        ctx.fillRect(i * 6, j * 6, 5, 5);
+        ctx.fillStyle = "#CB320F";
+        ctx.fillRect(i * 3, j * 3, 3, 3);
       } else {
-        ctx.fillStyle = "black";
-        ctx.fillRect(i * 6, j * 6, 5, 5);
+        ctx.fillStyle = "#11223C";
+        ctx.fillRect(i * 3, j * 3, 3, 3);
       }
     }
   }
-
-  setTimeout(function () {
-    update();
-  }, 3000);
 };
 
+// create seeded array
+// draw
+// create next array
+// set next array to original array
+// draw
 initialSeed();
-console.log(pixels);
+setTimeout(function () {
+  createNextArray();
+}, 60);
